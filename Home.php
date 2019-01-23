1bb260fd870111372592639a31942cde2e24fdfc
AldwynN <AldwynN@users.noreply.github.com>
2019-01-23 13:23:08 +0100

    Chapitre 1 - Création du projet et des pages principales
    Création des 2 pages Home et Post et création rapide d'un contenu sur la page Home

---
+++ Home.php
@@ -0,0 +1,31 @@
+<!-- 
+Titre : Page de profil de l'utilisateur
+Date : 23.01.2019
+Auteur : Romain Peretti
+Description : Cette page permettra d'afficher les informations de l'utilisateur ainsi que ce qu'il a publié (image/vidéos)
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='Post.php'>Post</a></li>
+            </ul>
+        </nav>
+        <!-- Section pour la photo de profile -->
+        <section>
+            <image src='https://fr.cdn.v5.futura-sciences.com/buildsv6/images/mediumoriginal/6/5/2/652a7adb1b_98148_01-intro-773.jpg' alt='Photo de profil' height='70' width='110'>
+            <h3>Romain Peretti</h3>
+        </section>
+        
+        <!-- Section pour le message d'accueil -->
+        <section>
+            <h1>Bienvenue sur ma page magnifique !</h1>
+        </section>
+    </body>
+</html>
---
+++ Post.php
@@ -0,0 +1,16 @@
+<!-- 
+Titre : ?
+Date : 23.01.2019
+Auteur : Romain Peretti
+Description : ?
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>
